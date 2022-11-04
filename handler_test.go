7feb8f39e936e6499726728e1732f1b98d66d151
stynore <setiyono.ressly@gmail.com>
2022-11-04 08:13:46 +0700

    add handler_test mux

--- handler_test.go
+++ handler_test.go
@@ -16,3 +16,27 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "Heloo world")
+	})
+
+	mux.HandleFunc("/hi", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "hai")
+	})
+	mux.HandleFunc("/images/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "image")
+	})
+
+	mux.HandleFunc("/images/thumbnail/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer, "thumbnail")
+	})
+
+	server := http.Server{Addr: "localhost:8080", Handler: mux}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
